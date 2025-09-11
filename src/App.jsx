e05697ab19da06c3dbf0b534ada171463e319886
chandan-xcoder <chaandanverma@gmail.com>
2025-09-11 09:24:19 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import Button from "./components/ui/Button";
 
 
 function App() {
